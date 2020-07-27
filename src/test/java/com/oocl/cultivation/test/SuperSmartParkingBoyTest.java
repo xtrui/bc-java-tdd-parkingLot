cd81756792480a8e862345d7e859e58bf47b6472
Ray <ray.wen@oocl.com>
2020-07-27 17:27:18 +0800

    [story5][ray] fix: fix should_return_parkingLot_which_has_larger_available_position_rate_when_getParkingLot test.

--- src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/SuperSmartParkingBoyTest.java
@@ -9,7 +9,7 @@ import java.util.LinkedHashSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SuperSmartParkingBoyTest {
+class SuperSmartParkingBoyTest {
     @Test
     void should_return_parkingLot_which_has_larger_available_position_rate_when_getParkingLot() {
         // given
@@ -17,8 +17,9 @@ public class SuperSmartParkingBoyTest {
         ParkingLot parkingLot1 = new ParkingLot(1, 12);
         ParkingLot parkingLot2 = new ParkingLot(2, 10);
         Car car = new Car(1, "car1");
+        Car car2 = new Car(2, "car2");
         parkingLot1.getCars().add(car);
-        parkingLot2.getCars().add(car);
+        parkingLot2.getCars().add(car2);
         superSmartParkingBoy.setParkingLot(parkingLot1);
         superSmartParkingBoy.setParkingLot(parkingLot2);
         // when
