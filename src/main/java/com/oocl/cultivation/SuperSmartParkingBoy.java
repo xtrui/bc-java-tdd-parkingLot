74bdae685b956860bb7f58bd21a1c171f125bf79
Ray <ray.wen@oocl.com>
2020-07-27 16:58:09 +0800

    [story123][ray] refactor: merge ParkingBoyStory1,2,3 to ParkingBoyTest

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -11,6 +11,7 @@ public class SuperSmartParkingBoy extends ParkingBoy {
     public ParkingLot getParkingLot() {
         double maxAvailablePositionRate = Double.MIN_VALUE;
         double availablePositionRate;
+
         for (ParkingLot parkingLot : parkingLots) {
             availablePositionRate = (parkingLot.getCapacity() - parkingLot.getCars().size()) / (double) parkingLot.getCapacity();
             if (availablePositionRate > maxAvailablePositionRate) {
--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ParkingBoyFacts {
+class ParkingBoyTest {
 
     @Test
     void should_return_ticket_when_parking_given_car() {
@@ -85,5 +85,69 @@ class ParkingBoyFacts {
         Ticket ticket = parkingBoy.park(car);
         //then
         assertNull(ticket);
+    }
+
+    @Test
+    void should_return_appropriate_errMessage_when_park_err_and_getErrMessage() {
+        // given
+        ParkingBoy parkingBoy = new ParkingBoy(new LinkedHashSet<>(), new ParkingLot(1, 10));
+        Car car = new Car(1, "car1");
+        // when
+
+        parkingBoy.park(car);
+        Ticket ticket = parkingBoy.park(car);
+        String errMessage = parkingBoy.getErrMessage();
+        parkingBoy.park(null);
+        String errMessage2 = parkingBoy.getErrMessage();
+        List<Car> cars = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            cars.add(new Car(i, "car" + i));
+        }
+        parkingBoy.getParkingLot().setCars(cars);
+        parkingBoy.park(car);
+        String errMessage3 = parkingBoy.getErrMessage();
+        // then
+        assertNull(ticket);
+        assertEquals("这辆车已经在停车场了。", errMessage);
+        assertEquals("你没有给车!", errMessage2);
+        assertEquals("Not enough position.", errMessage3);
+
+    }
+
+    @Test
+    void should_return_appropriate_errMessage_when_fetch_given_err_ticket() {
+        // given
+        ParkingBoy parkingBoy = new ParkingBoy(new LinkedHashSet<>(), new ParkingLot(1, 10));
+        Ticket ticket = new Ticket(11);
+
+        // when
+        parkingBoy.fetch(ticket);
+        String errMessage = parkingBoy.getErrMessage();
+        parkingBoy.fetch(null);
+        String errMessage2 = parkingBoy.getErrMessage();
+
+        //then
+        assertEquals(errMessage, "Unrecognized parking ticket.");
+        assertEquals(errMessage2, "Please provide your parking ticket.");
+
+    }
+
+    @Test
+    void should_return_2_when_getParkingLot_given_11_cars() {
+        // given
+        ParkingBoy parkingBoy = new ParkingBoy(new LinkedHashSet<>(), new ParkingLot(0, 10));
+        ParkingLot parkingLot2 = new ParkingLot(2);
+        Car car = new Car(11, "car1");
+        // when
+        List<Car> cars = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            cars.add(new Car(i, "car" + i));
+        }
+        parkingBoy.getParkingLot().setCars(cars);
+        parkingBoy.setParkingLot(parkingLot2);
+        parkingBoy.park(car);
+        ParkingLot currentParkingLot = parkingBoy.getParkingLot();
+        // then
+        assertEquals(currentParkingLot, parkingLot2);
     }
 }
